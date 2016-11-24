cc9596fbf76d4e3dcf252f1857c4b60c4d783fac
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2016-11-24 11:42:11 +0100

    no need for babel, OpenWhisk is using a recent node version

--- webpack.config.js
+++ webpack.config.js
@@ -32,15 +32,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.json$/, loader: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-        },
-      },
+      { test: /\.json$/, loader: 'json-loader' }
     ],
   },
   target: 'node',
